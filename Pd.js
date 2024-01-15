aa4fef5b2141056d7a391e9e0b1e8e41b4252700
Wagenknecht711 <155693723+Wagenknecht711@users.noreply.github.com>
2024-01-15 20:34:59 +1100

    Update Pd.js

--- Pd.js
+++ Pd.js
@@ -12,12 +12,9 @@ function OnStart()
 	btn1= app.CreateButton( "on", 0.23, 0.05 ) 
 	btn1.SetOnTouch( btn1_OnTouch ) 
 	layBut.AddChild( btn1) 
-	btn2= app.CreateButton( "0", 0.23, 0.1 ) 
+	btn2= app.CreateButton( "reveal", 0.23, 0.1 ) 
 	btn2.SetOnTouch( btn2_OnTouch ) 
 	lay.AddChild( btn2) 
-	btn3= app.CreateButton( "1", 0.23, 0.1 ) 
-	btn3.SetOnTouch( btn3_OnTouch ) 
-	lay.AddChild( btn3) 
 	btn4= app.CreateButton( "randomize", 0.23, 0.1 ) 
 	btn4.SetOnTouch( btn4_OnTouch ) 
 	lay.AddChild( btn4) 
@@ -34,44 +31,31 @@ numa += b ;
 function btn0_OnTouch() 
 { 
 
-choiceX == "0"
+choiceX = "0"
 app.ShowPopup("off" );
 } 
 
 function btn1_OnTouch() 
 { 
 
-choiceX == "1"
+choiceX = "1"
 app.ShowPopup("on" );
 } 
 
 function btn2_OnTouch() 
 { 
 
+
+
 if (choiceX == "0"){
-choice = numa[i]
-i+=1
+app.ShowPopup("off")
 }
 if (choiceX == "1"){
-choice = Math.floor(Math.random() * (2)) + 0;
+app.ShowPopup("on")
 }
-app.ShowPopup(choice );
 
 } 
 
-function btn3_OnTouch() 
-{ 
-
-if (choiceX == "0"){
-choice = numa[i]
-i+=1
-}
-if (choiceX == "1"){
-choice = Math.floor(Math.random() * (2)) + 0;
-}
-app.ShowPopup(choice );
-
-} 
 
 
 function btn4_OnTouch() 
